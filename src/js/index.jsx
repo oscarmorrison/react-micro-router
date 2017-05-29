524be3093c2755f59e89bd3e0f319ba3187e373e
oscarmorrison <me@oscarmorrison.com>
2017-05-29 00:35:29 -0400

    basic router component up and running

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,45 +1,64 @@
+/* eslint no-console: 0 */
 import React from 'react';
 import { render } from 'react-dom';
+import createHistory from 'history/createBrowserHistory';
 
 import '../style/index.scss';
 
 class App extends React.Component {
 
-    getWelcomeText = () => {
-        return 'React Webpack Starter';
+    routes = () => ({
+        '/': 'home',
+        '/about': 'about',
+    })
+
+    home = () => {
+        return <div>home</div>;
     }
 
-    render() {
-        const welcomeText = this.getWelcomeText();
-        return (
-            <div>
-                <h1>{welcomeText}</h1>
-            </div>
-        );
+    about = () => {
+        return <div>about</div>;
     }
+
 }
+
+const history = createHistory();
+
+const _404 = () => {
+    return <div><h1>404</h1><p>Route not found</p></div>;
+};
+
+const getRouteNameFromHash = hash => hash.replace('#', '/') || '/';
 
 const Router = Component => class extends Component {
 
     constructor(props) {
         super(props);
+        const routeName = getRouteNameFromHash(window.location.hash);
+        this.state = { routeName };
         this.registerListener();
     }
 
     registerListener = () => {
-        window.addEventListener('hashchange', this.routeChanged);
+        this.unlisten = history.listen(this.routeChanged);
     }
 
     componentWillUnmount = () => {
-        window.removeEventListener('hashchange', this.routeChanged);
+        this.unlisten();
     }
 
-    routeChanged = (e) => {
-        console.log('routeChanged', e);
+    routeChanged = (location, action) => {
+        console.log('routeChanged', location, action);
+        const routeName = getRouteNameFromHash(location.hash);
+        this.setState({ routeName });
     }
 
     render() {
-        return super.render();
+        const { routeName } = this.state;
+        const routes = this.routes();
+        const componentName = routes[routeName] || routes['/_404'];
+        const RouteComponent = this[componentName] || _404;
+        return <RouteComponent {...this.props} />;
     }
 };
 
